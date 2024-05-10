d1456142812b583a6e41bcd55cb165ec551ef6d6
santy <santicinel@gmail.com>
2024-05-10 14:14:29 -0300

    practica 5

--- practico_05/ejercicio_01.py
+++ practico_05/ejercicio_01.py
@@ -14,6 +14,39 @@ class Socio(Base):
         - apellido: string (longitud 250)
     """
     __tablename__ = 'socios'
+    
+    
+    id_socio=Column(Integer,primary_key=True, autoincrement=True)
+    dni=Column(Integer,unique=True,nullable=False)
+    nombre=Column(String(250),nullable=False)
+    apellido=Column(String(250),nullable=False)
+    
+    
+    def asignar(self,otro):
+        if isinstance(otro,Socio):
+            atributos=list(self.__dict__.keys())
+            atributos.remove('_sa_instance_state')
+            for attr in atributos: 
+                setattr(self,attr,getattr(otro,attr))
+        else: 
+            raise ValueError("Debe ser un objeto del tipo Socio")
+        
+    def __repr__(self):
+        return f"Socio(dni={self.dni},nombre={self.nombre},apellido={self.apellido})"
+
+    def __str__(self):
+        return f"Socio nombre:{self.nombre}, apellido:{self.apellido}, dni:{self.dni}, id_socio={self.id_socio}"
+
+    def __eq__(self,otro):
+        if isinstance(otro,Socio):
+            equivalente=True
+            atributos=list(self.__dict__.keys())
+            atributos.remove('_sa_instance_state')
+            for attr in atributos:
+                equivalente=equivalente and getattr(self,attr)==getattr(otro,attr)
+            return equivalente
+        else:
+            raise ValueError("Debe ser objeto tipo socio")
 
     # Completar
 
--- practico_05/ejercicio_02.py
+++ practico_05/ejercicio_02.py
@@ -3,55 +3,99 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from ejercicio_01 import Base, Socio
+from typing import List, Optional
+from sqlalchemy.orm.exc import UnmappedInstanceError
+
 
 from typing import List, Optional
 
 class DatosSocio():
 
     def __init__(self):
-        pass # Completar
+        self.__engine=create_engine("sqlite:///prueba.db")
+        self.__Session = sessionmaker(bind=self.__engine)
+        Base.metadata.create_all(bind = self.__engine)
 
     def buscar(self, id_socio: int) -> Optional[Socio]:
         """Devuelve la instancia del socio, dado su id. Devuelve None si no 
         encuentra nada.
         """
-        pass # Completar
+        sesion=self.__Session()
+        socio=sesion.query(Socio).filter(Socio.id_socio==id_socio).first()
+        sesion.close()
+        return socio
 
     def buscar_dni(self, dni_socio: int) -> Optional[Socio]:
         """Devuelve la instancia del socio, dado su dni. Devuelve None si no 
         encuentra nada.
         """
-        pass # Completar
+        sesion=self.__Session()
+        socio=sesion.query(Socio).filter(Socio.dni==dni_socio).first()
+        sesion.close()
+        return socio
         
     def todos(self) -> List[Socio]:
         """Devuelve listado de todos los socios en la base de datos."""
-        pass # Completar
+        sesion=self.__Session()
+        socio=sesion.query(Socio).all()
+        sesion.close()
+        return socio
 
     def borrar_todos(self) -> bool:
         """Borra todos los socios de la base de datos. Devuelve True si el 
         borrado fue exitoso.
         """
-        pass # Completar
+        sesion=self.__Session()
+        sesion.query(Socio).delete()
+        sesion.commit()
+        borrados=sesion.deleted
+        sesion.close()
+        
+        return not bool(borrados)
 
     def alta(self, socio: Socio) -> Socio:
         """Agrega un nuevo socio a la tabla y lo devuelve"""
-        pass # Completar
+        sesion=self.__Session(expire_on_commit=False)
+        sesion.add(socio)
+        sesion.commit()
+        sesion.expunge(socio)
+        sesion.close()
+
 
     def baja(self, id_socio: int) -> bool:
         """Borra el socio especificado por el id. Devuelve True si el borrado 
         fue exitoso.
         """
-        pass # Completar
+        sesion=self.__Session(expire_on_commit=False)
+        socio=sesion.query(Socio).filter(Socio.id_socio==id_socio).first()
+        if socio:
+            sesion.delete(socio)
+            sesion.commit()
+        else: 
+            raise UnmappedInstanceError
+        sesion.close()
+        return bool(socio)
 
     def modificacion(self, socio: Socio) -> Socio:
         """Guarda un socio con sus datos modificados. Devuelve el Socio 
         modificado.
         """
-        pass # Completar
+        sesion = self.__Session()
+        socio_viejo=sesion.query(Socio).filter(Socio.id_socio==socio.id_socio).first()
+        if socio_viejo:
+            socio_viejo.asignar(socio)
+            sesion.commit()
+        else:
+            raise UnmappedInstanceError
+        sesion.close()
+        return socio
     
     def contarSocios(self) -> int:
         """Devuelve el total de socios que existen en la tabla"""
-        pass # Completar
+        sesion=self.__Session()
+        cant_personas=sesion.query(Socio).count()
+        sesion.close()
+        return cant_personas
 
 
 
@@ -61,22 +105,22 @@ class DatosSocio():
 datos = DatosSocio()
 
 # Test Alta
-socio = datos.alta(Socio(dni=12345678, nombre='Juan', apellido='Perez'))
-assert socio.id > 0
+socio = datos.alta(Socio(dni=1234567, nombre='Juan', apellido='Perez'))
+assert socio.id_socio > 0
 
 # Test Baja
-assert datos.baja(socio.id) == True
+assert datos.baja(socio.id_socio) == True
 
 # Test Consulta
-socio_2 = datos.alta(Socio(dni=12345679, nombre='Carlos', apellido='Perez'))
+socio_2 = datos.alta(Socio(dni=1234567, nombre='Carlos', apellido='Perez'))
 assert datos.buscar(socio_2.id) == socio_2
 
 # Test Buscar DNI
-socio_2 = datos.alta(Socio(dni=12345670, nombre='Carlos', apellido='Perez'))
+socio_2 = datos.alta(Socio(dni=1234567, nombre='Carlos', apellido='Perez'))
 assert datos.buscar_dni(socio_2.dni) == socio_2
 
 # Test Modificación
-socio_3 = datos.alta(Socio(dni=12345680, nombre='Susana', apellido='Gimenez'))
+socio_3 = datos.alta(Socio(dni=1234568, nombre='Susana', apellido='Gimenez'))
 socio_3.nombre = 'Moria'
 socio_3.apellido = 'Casan'
 socio_3.dni = 13264587
